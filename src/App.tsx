f956bda79304c7d6bdb365bad6eb7821101378ac
Luca <heredialuca06@gmail.com>
2026-09-22 17:19:05 -0300

    Primero commit: proyecto Vite + React + TS inicial

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+    return (
+      <div>
+        <h1>Synar</h1>
+        <p>Bienvenido, Luca</p>
+        <div>
+          <div>Ventas: $0</div>
+          <div>Productos: 0</div>
+          <div>Stock: 0</div>
+        </div>
+      </div>
+    )
+}
+
+export default App
