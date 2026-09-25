348e3f5ab246ec8b0cd42814b9117f3e4feb80d5
Luca <heredialuca06@gmail.com>
2026-09-25 03:45:48 -0300

    Agregar funcion createProduct expuesta via IPC, con tipos correctos

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,5 +1,7 @@
 import { contextBridge, ipcRenderer } from "electron"
+import type { Product } from './db/schema'
 
 contextBridge.exposeInMainWorld('api', {
-    getProducts: () => ipcRenderer.invoke('get-products')
+    getProducts: () => ipcRenderer.invoke('get-products'),
+    createProduct: (product: Omit<Product, 'id'>) => ipcRenderer.invoke('create-product', product)
 })
